292fdcc2a6a0178e0cf4ea9ad33876d9fdd3b29e
sharanya <116085931+sharanyazx@users.noreply.github.com>
2024-12-28 08:21:49 -0800

    Time: 1 ms (96.81%) | Memory: 45.8 MB (25.22%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -17,4 +17,45 @@ class Solution {
     }
     
 }
-            
+           
+           
+           
+           
+           
+            // class Solution {
+//     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+//         int i =0;
+//         int length=flowerbed.length;
+//             while(n > 0 && i < length){
+//                  if(flowerbed[i]==1) { 
+//                     i=i+2;
+//                     }
+//                     else if(i==length-1 || flowerbed[i+1]==0){
+//                         n--;
+//                         i=i+2;
+//                     }
+//                     else i=1+3;
+
+               
+//                 }
+//                 return n<=0;
+//         }
+        
+//     }
+    
+
+//             // int i =0;
+//             // while(n>0 && i<flowerbed.length()){
+//             //      if(flowerbed[i]==1) { 
+//             //         i=i+2;
+//             //         }
+//             //         else if(i==len-1 or flowerbed[i+1]==0){
+//             //             n--;
+//             //             i=i+2;
+//             //         }
+//             //         else i=1+3;
+
+               
+//             //     }
+//             //     return n<=0;
+           
