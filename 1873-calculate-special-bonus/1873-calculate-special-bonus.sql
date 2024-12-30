6fc4fb7d7d37d15ce44827186716412632f0d718
sharanya <116085931+sharanyazx@users.noreply.github.com>
2024-12-30 09:10:26 -0800

    Time: 846 ms (31.13%), Space: 0B (100%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -2,6 +2,6 @@ select employee_id,
         case 
            when employee_id % 2 != 0 and left(name, 1) != 'M' then salary
            else 0
-        end AS bonus
+       End AS bonus
 from Employees
 order by employee_id;
