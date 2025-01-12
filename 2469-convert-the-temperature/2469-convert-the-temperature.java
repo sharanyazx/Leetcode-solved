f2e2446d1806dbb8a30b8302feb301a1e84dfaa4
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-01-11 23:25:27 -0800

    Time: 0 ms (100%), Space: 42 MB (35.59%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+       double kelvin= celsius + 273.15;
+       double  fahrenheit=celsius * 1.80 + 32.00;
+       double [] result = {Math.round(kelvin * 100000.0) / 100000.0, Math.round(fahrenheit * 100000.0) / 100000.0};
+      return result;
+
+    
+    }
+}
