2daade313a27401cbb8de36bf2c742e27eaaa727
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-01-12 02:08:05 -0800

    Time: 1 ms (86.99%), Space: 41.2 MB (27.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,26 @@
-// class Solution {
-//     public int mySqrt(int x) {
-//         if (x == 0) return 0;
-//         int left = 1, right = x;
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             if (mid == x / mid) {
-//                 return mid;
-//             } else if (mid < x / mid) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-//         return right;
-//     }
-// }
 class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+}
+ 
+ // With the built in function
+
+/*class Solution {
     public int mySqrt(int x) {
         double res=Math.pow(x,0.5);
         return (int) res;
     }
-}
+}*/
