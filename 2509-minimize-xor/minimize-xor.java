249236f519df8ffadd749b89e932b9cc158b8f56
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-01-15 11:03:23 -0800

    Time: 0 ms (100.00%) | Memory: 40.9 MB (21.70%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public int findSetBits(int num2) {
+        int counter = 0;
+        while (num2 != 0) {
+            num2 = (num2 & (num2 - 1));
+            counter++;
+        }
+        return counter;
+    }
+
+    public int minimizeXor(int num1, int num2) {
+        int numSetBits = findSetBits(num2);
+        int result = 0;
+        
+        for (int i = 31; i >= 0; i--) {
+            if ((num1 & (1 << i)) != 0 && numSetBits > 0) {
+                result |= (1 << i);
+                numSetBits--;
+            }
+        }
+        
+        int index = 0;
+        while (numSetBits > 0) {
+            if ((result & (1 << index)) == 0) {
+                result |= (1 << index);
+                numSetBits--;
+            }
+            index++;
+        }
+        
+        return result;
+    }
+}
