4e7603d4d1d8ce288dc7ae75c48c23b5c54d1749
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-01-16 08:54:52 -0800

    Time: 413 ms (57.17%), Space: 66.5 MB (95.08%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+     grouped = actor_director.groupby(['actor_id', 'director_id']).size().reset_index(name='cooperation_count')
+     filtered_pairs = grouped[grouped['cooperation_count'] >= 3]
+     return filtered_pairs[['actor_id', 'director_id']]
