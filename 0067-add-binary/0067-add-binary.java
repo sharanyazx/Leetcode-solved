1aef8902875b3f8ff6c8436c0f3bf65324de917c
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-01-16 21:46:09 -0800

    Time: 5 ms (17.86%), Space: 42.9 MB (15.93%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,20 +1,10 @@
-import java.math. BigInteger;
-import java.util.Scanner;
+import java.math.BigInteger;
+
 class Solution {
-    public  String addBinary(String a, String b) {
-   BigInteger num1 =  new BigInteger(a,2);
-     BigInteger num2=new BigInteger(b,2);
-      BigInteger sum=num1.add(num2);
-    
-   return sum.toString(2);
-        
-    }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-         String binary1=sc.nextLine();
-              String binary2=sc.nextLine();
-        Solution op=new Solution();
-       String sum = op.addBinary(binary1,binary2);
-        sc.close();
+    public String addBinary(String a, String b) {
+        BigInteger first = new BigInteger(a, 2);
+        BigInteger second = new BigInteger(b, 2);
+        var result = first.add(second);
+        return result.toString(2);
     }
 }
