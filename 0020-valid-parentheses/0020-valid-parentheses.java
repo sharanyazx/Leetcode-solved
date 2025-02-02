32c0be63a974df215a116f021e7167293ab0414e
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-02 22:57:50 +0530

    Time: 2 ms (97.66%), Space: 41.7 MB (81.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                if ((c == ')' && stack.peek() != '(') ||
+                    (c == ']' && stack.peek() != '[') ||
+                    (c == '}' && stack.peek() != '{')) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
