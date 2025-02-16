a1660879737a5ec11335dc3f1d916660d8f1e9de
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-16 14:22:48 +0530

    Time: 1 ms (97.17%), Space: 43.1 MB (64.14%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,20 +1,36 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        long max1=Long.MIN_VALUE;
-        long max2=Long.MIN_VALUE;
-        long max3=Long.MIN_VALUE;
-        for(int num : nums){
-            if(num>max1){
-                max3 = max2;
-                max2 = max1;
-                max1 = num;
-            }else if(max1>num && num>max2){
-                max3 = max2;
-                max2 = num;
-            }else if(max2>num && num>max3){
-                max3=num;
+        int a = Integer.MIN_VALUE;
+        int b = Integer.MIN_VALUE;
+        int c = Integer.MIN_VALUE;
+        boolean aa = false;
+        boolean bb = false;
+        boolean cc = false;
+        for(int n : nums){
+            if(n > a || !aa){
+                if(aa){
+                    if(bb){
+                        c = b;
+                        cc = true;
+                    }else
+                        bb = true;
+                    b  = a;
+                }else
+                    aa = true;
+                a = n;
+            }else if(n > b && n < a || !bb && n < a){
+                if(bb){
+                    c = b;
+                    cc = true;
+                }else
+                    bb = true;
+                b = n;
+            }else if(n > c && n < b || !cc && n < b){
+                cc = true;
+                c = n;
             }
         }
-        return max3 != Long.MIN_VALUE ? (int) max3 : (int) max1;
+
+        return !cc ? a : c;
     }
 }
