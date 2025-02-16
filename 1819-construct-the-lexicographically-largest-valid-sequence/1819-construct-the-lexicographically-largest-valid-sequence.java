611485bc4111742a706ccff9db20d798ec7035b5
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-16 12:06:33 +0530

    Time: 0 ms (100%), Space: 41 MB (67.88%) - LeetHub

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean fun(int[] arr, boolean[] vis, int ind, int n) {
+        if (ind == arr.length) {
+            return true;
+        }
+        if (arr[ind] != 0) {
+            return fun(arr, vis, ind + 1, n);
+        }
+        for (int i = n; i >= 1; i--) {
+            if (vis[i]) {
+                continue;
+            }
+            int nextInd = (i == 1) ? ind : ind + i;
+            if (nextInd >= arr.length || arr[nextInd] != 0) {
+                continue;
+            }
+            arr[ind] = i;
+            arr[nextInd] = i;
+            vis[i] = true;
+            if (fun(arr, vis, ind + 1, n)) {
+                return true;
+            }
+            arr[ind] = 0;
+            arr[nextInd] = 0;
+            vis[i] = false;
+        }
+        return false;
+    }
+
+    public int[] constructDistancedSequence(int n) {
+        int l = 2 * n - 1;
+        int[] arr = new int[l];
+        boolean[] vis = new boolean[n + 1];
+        fun(arr, vis, 0, n);
+        return arr;
+    }
+}
