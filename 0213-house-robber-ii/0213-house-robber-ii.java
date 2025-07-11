418a0b80fcdb96d0ddc284675471a74040dccb24
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-07-10 22:38:01 -0700

    Time: 0 ms (100%), Space: 41.2 MB (38.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,18 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        return Math.max(getMax(nums, 0, nums.length - 2), getMax(nums, 1, nums.length - 1));        
+        int size=nums.length;
+        if(size==1)
+            return nums[0];
+        else if(size==2)
+            return Math.max(nums[0],nums[1]);
+        int res1 = maxCost(nums, 0, size-1);
+        int res2 = maxCost(nums, 1, size-1);
+        return Math.max(res1,res2);
     }
 
-    private int getMax(int[] nums, int start, int end) {
-        int prevRob = 0, maxRob = 0;
-
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(maxRob, prevRob + nums[i]);
-            prevRob = maxRob;
-            maxRob = temp;
+    int maxCost(int[] nums,int st, int size){
+        // int end = st + size;
+        int[] dpCost = new int[size];
+        if(size == 1)
+            return nums[st];
+        dpCost[0] = nums[st];
+        dpCost[1] = Math.max(nums[st],nums[st+1]);
+        for(int i=2;i<size;i++){
+            dpCost[i] = Math.max(dpCost[i-1],dpCost[i-2]+nums[i+st]);
         }
-
-        return maxRob;
-    }    
+        return dpCost[size-1];
+    }
 }
