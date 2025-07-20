79395eae74109526f9226fa83686c86d63984adc
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-07-20 21:59:30 +0530

    Time: 2 ms (98.70%) | Memory: 43 MB (98.38%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,7 +3,8 @@ class Solution {
         int maxLen = 0;
         int n=128;
         int[] arr = new int[n];
-        for(int i=0,j=0;j<s.length();j++){
+        int i=0;
+        for(int j=0;j<s.length();j++){
             i = Math.max(arr[s.charAt(j)],i);
             maxLen = Math.max(maxLen,-i+1+j);
             arr[s.charAt(j)] = j+1;
