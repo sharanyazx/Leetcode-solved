5e89735011a05326f7d9a372819a53f4b0869246
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-07-28 12:29:06 +0530

    Time: 53 ms (67.93%), Space: 60.3 MB (35.16%) - LeetHub

---
+++ 1305-number-of-visible-people-in-a-queue/1305-number-of-visible-people-in-a-queue.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        int n = heights.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();  // Monotonic decreasing stack
+
+        for (int i = n - 1; i >= 0; i--) {
+            int visible = 0;
+
+            // See all shorter people
+            while (!stack.isEmpty() && heights[i] > stack.peek()) {
+                stack.pop();
+                visible++;
+            }
+
+            // Can see the next taller/equal person too (if exists)
+            if (!stack.isEmpty()) {
+                visible++;
+            }
+
+            res[i] = visible;
+            stack.push(heights[i]);
+        }
+
+        return res;
+    }
+}
