5e6cf7fc765d96a2ebfa0755e3785ddfdc1e7547
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-08-04 10:24:27 +0530

    Time: 41 ms (42.82%), Space: 18 MB (16.88%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,11 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            reversed_num = -int(str(-x)[::-1])
-        else:
-            reversed_num = int(str(x)[::-1])
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        res = 0
+        num = abs(x)
         
-        # Check for 32-bit signed integer range
-        if reversed_num < -2**31 or reversed_num > 2**31 - 1:
-            return 0
-        return reversed_num
+        while num:
+            digit = num % 10
+            num //= 10
+
+            # Check overflow
+            if res > (INT_MAX - digit) // 10:
+                return 0
+
+            res = res * 10 + digit
+        
+        return res if x >= 0 else -res
