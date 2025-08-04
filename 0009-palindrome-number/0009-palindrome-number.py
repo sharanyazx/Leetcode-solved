4f7bb2fc871628d5746ce23974c202e7e59d7de3
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-08-04 10:33:41 +0530

    Time: 4 ms (80.25%), Space: 17.8 MB (64.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,18 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x<0:
+#             return False
+#         else:
+#             rev=int(str(x)[::-1])
+            
+#         if(x==rev): return True
+#         else: return False
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
-            return False
+        a=str(x)
+        if a == a[::-1]:
+            return True
         else:
-            rev=int(str(x)[::-1])
+            return False
             
-        if(x==rev): return True
-        else: return False
-        
+         
