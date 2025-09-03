f92a1c0c92c3c27199b39eea884ef4762f6bef8b
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-09-03 10:48:29 +0530

    Create 978-ValidMountainArray

---
+++ 978-ValidMountainArray/978-ValidMountainArray.java
@@ -0,0 +1,22 @@
+// Last updated: 9/3/2025, 10:48:25 AM
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+      int i = 0, n = arr.length;
+      // Ascending part (Increasing)
+        while (i < n - 1 && arr[i] < arr[i + 1]) {
+            i++;
+        }
+        // Peak should not be first or last element
+        if (i == 0 || i == n - 1) return false;
+
+        // Descending part (Decreasing)
+        while (i < n - 1 && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        // If we reach the end, it's a valid mountain
+        return i == n - 1;
+    }
+}
+
+ 
