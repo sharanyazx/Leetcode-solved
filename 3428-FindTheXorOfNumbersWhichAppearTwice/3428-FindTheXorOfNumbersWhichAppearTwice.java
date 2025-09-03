dcd70b3a4d7a3ec23191e55bd245cd6bceb6cb46
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-09-03 10:46:06 +0530

    Create 3428-FindTheXorOfNumbersWhichAppearTwice

---
+++ 3428-FindTheXorOfNumbersWhichAppearTwice/3428-FindTheXorOfNumbersWhichAppearTwice.java
@@ -0,0 +1,18 @@
+// Last updated: 9/3/2025, 10:46:03 AM
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int result = 0;
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() > 1) {
+                result ^= entry.getKey();
+            }
+        }
+
+        return result;
+    }
+}
