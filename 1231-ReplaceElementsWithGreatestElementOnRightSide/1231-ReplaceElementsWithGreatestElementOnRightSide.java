c743ecf317b21d5d8aadfee9808f09b240933775
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-09-03 10:48:10 +0530

    Create 1231-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1231-ReplaceElementsWithGreatestElementOnRightSide/1231-ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,14 @@
+// Last updated: 9/3/2025, 10:48:06 AM
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        int last = -1; // Last element is always replaced with -1
+        for (int i = n - 1; i >= 0; i--) { // Traverse from right to left
+            int temp = arr[i]; // Store current element
+            arr[i] = last; // Replace with maxRight
+            last= Math.max(last, temp); // Update maxRight
+        }
+
+        return arr;
+    }
+}
