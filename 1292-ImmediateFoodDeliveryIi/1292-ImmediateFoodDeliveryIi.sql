d1d828cb84f7c8b1c596b6afccaccaf457381dc5
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-09-03 10:48:03 +0530

    Create 1292-ImmediateFoodDeliveryIi

---
+++ 1292-ImmediateFoodDeliveryIi/1292-ImmediateFoodDeliveryIi.sql
@@ -0,0 +1,10 @@
+-- Last updated: 9/3/2025, 10:48:00 AM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery where (customer_id, order_date) in 
+(
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
